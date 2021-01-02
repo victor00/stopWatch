660af659fa9a020ebe90908952ee11859ba1d0b2
victor <victor.morelli.007@gmail.com>
2021-01-01 23:52:18 -0300

    Toggle Background and Font Color working

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import Toggle from './components/Toggle/index';
 
 function App() {
   
+  const black = '#000000';
+  const white = '#FFFFFF';
+
   const [toggled, setToggled] = React.useState(false);
-  const [background, setBackground] = React.useState("#fdfdfd");
-  const [font, setFont] = React.useState("#424246");
+  const [background, setBackground] = React.useState(white);
+  const [color, setColor] = React.useState(black);
+  
+  
+
 
 
   const handleClick = () => {
     setToggled((s) => !s); //inverter valor antigo
-    setBackground(background);
-    setFont(font);
+    
+    if(background == white) {
+      setBackground(black);
+      setColor(white);
+    } else {
+      setBackground(white);
+      setColor(black);
+    }
+    
   };
 
 
  
 
   return (
-    <div className="App">
+    <div className="App" style={{background, color}}>
       <header className="App-header">
         
         <div className='theme-options'>
-          <Toggle toggled={toggled} onClick={handleClick} />
+          <Toggle toggled={toggled} onClick={handleClick}  />
           <Clock />
         </div>
 
--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -3,21 +3,23 @@ import "./style.css";
 
 
 export default function Toggle({toggled, onClick}) {
-    return (
     
-        <div onClick={onClick} className={`toggle${toggled ? " night" : ""}`}>
-            <div className="notch"> 
-                <div className="crater" />
-                <div className="crater" />
+    
+    return (
+        <>
+            <div onClick={onClick} className={`toggle${toggled ? " night": ""}`} >
+                <div className="notch"> 
+                    <div className="crater" />
+                    <div className="crater" />
+                </div>
+                <div>
+                    <div className="shape sm" />
+                    <div className="shape sm" />
+                    <div className="shape md" />
+                    <div className="shape lg" />
+                </div>
             </div>
-            <div>
-                <div className="shape sm" />
-                <div className="shape sm" />
-                <div className="shape md" />
-                <div className="shape lg" />
-            </div>
-        </div>
-
+        </>
     );
 }
 
